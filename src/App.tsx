e3ac8db9f04710125536d9923d0c6dda9d6f927a
Barbara Contardi <barbi.contardi@gmail.com>
2023-11-19 12:46:08 +0100

    feat: background

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import '../src/style/index.scss'
+import ButtonUsage from './component/Button'
+
+
+function App() {
+
+
+  return (
+    <>
+   <div>
+    <p> Este es mi blog</p>
+  <ButtonUsage/>
+   </div>
+    </>
+  )
+}
+
+export default App
