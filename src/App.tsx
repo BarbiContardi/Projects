45ac804b171c54be88ab513a47481b70b2e8ac5b
Barbara Contardi <barbi.contardi@gmail.com>
2023-11-25 13:15:31 +0100

    primeros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import "./App.css";
 import "../src/style/index.scss";
 import Button from "./component/Button";
-
+import gsap from "gsap";
+import { useState } from "react";
 
 function App() {
+  const [visible, setVisible] = useState(null);
+  const handleClick = () => {
+    setVisible
+      gsap.to(".box1", {
+        scrollTrigger: ".box1",
+        x: 300,
+      });
+  };
   return (
-    <>
-      <div>
-        <p> Este es mi blog</p>
-        <Button isPrimary={true} onClick={()=>{}} texto={"Hola"}/>
-      </div>
-    </>
+    <div className="principal">
+      <div className="container" onClick={handleClick}>
+        <Button isPrimary={true} onClick={() => {visible}} texto={"Perfil"} />
+        <Button isPrimary={true} onClick={() => {visible}} texto={"Proyectos"} />
+        <Button isPrimary={true} onClick={() => {visible}} texto={"Contacto"} />
+        <Button isPrimary={true} onClick={() => {visible}} texto={"CV"} />
+        </div>
+
+        <div className="box1">
+          <p>Perfil</p>
+        </div>
+
+    </div>
   );
 }
 
